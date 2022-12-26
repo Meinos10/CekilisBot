d06d964d9dcec72d101d3b91a12c9d5199c1d02a
ReWoxi <64690769+Meinos10@users.noreply.github.com>
2022-12-26 19:38:13 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-api_id = 18170822
-api_hash = "ef37e3822315ab88a12d65589bd83c0c"
-token = "5711315584:AAH4W2qaAMigABHNHwpBSA02Ae-XrZJJ6yY"
+api_id = # my.telegram.org/app app_id
+api_hash = "" # my.telegram.org/app api_hash
+token = "" # Telegram Bot token
 
-sudo = [1006893341]
+sudo = []
 sudo.append(1339844465)
